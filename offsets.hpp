b5be09074993526c890ef588f274a67fd6965205
belgium <keygan900@gmail.com>
2026-09-16 23:07:15 -0400

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -5,9 +5,9 @@
 /* -------------------------------------------------------------
 /*  Dumped With     : RbxDumperV2                               
 /*  Source code     : https://git.imtheo.lol/theo/RbxDumperV2   
-/*  Roblox Version  : version-c5aecda2245e4fae
-/*  Dumper Version  : 2.2.3
-/*  Dumped At       : 17:54 09/09/2026 (GMT)
+/*  Roblox Version  : version-4310300497aa4917
+/*  Dumper Version  : 2.2.4
+/*  Dumped At       : 06:24 15/09/2026 (GMT)
 /*  Total Offsets   : 392
 /* -------------------------------------------------------------
 /*  Join the discord!                                           
@@ -18,7 +18,7 @@
 #include <cstdint>
 #include <string>
 namespace Offsets {
-    inline std::string ClientVersion = "version-c5aecda2245e4fae";
+    inline std::string ClientVersion = "version-4310300497aa4917";
 
     namespace AirProperties {
          inline constexpr uintptr_t AirDensity = 0x18;
@@ -96,7 +96,7 @@ namespace Offsets {
     }
 
     namespace CachedItem {
-         inline constexpr uintptr_t FileMeshData = 0x28;
+         inline constexpr uintptr_t FileMeshData = 0x40;
     }
 
     namespace Camera {
@@ -140,19 +140,19 @@ namespace Offsets {
     }
 
     namespace DataModel {
-         inline constexpr uintptr_t CreatorId = 0x180;
-         inline constexpr uintptr_t GameId = 0x188;
-         inline constexpr uintptr_t GameLoaded = 0x5d8;
-         inline constexpr uintptr_t JobId = 0x118;
-         inline constexpr uintptr_t PlaceId = 0x190;
-         inline constexpr uintptr_t PlaceVersion = 0x1ac;
-         inline constexpr uintptr_t PrimitiveCount = 0x420;
+         inline constexpr uintptr_t CreatorId = 0x188;
+         inline constexpr uintptr_t GameId = 0x190;
+         inline constexpr uintptr_t GameLoaded = 0x5e0;
+         inline constexpr uintptr_t JobId = 0x120;
+         inline constexpr uintptr_t PlaceId = 0x198;
+         inline constexpr uintptr_t PlaceVersion = 0x1b4;
+         inline constexpr uintptr_t PrimitiveCount = 0x428;
          inline constexpr uintptr_t ScriptContext = 0x440;
-         inline constexpr uintptr_t ServerIP = 0x5c0;
-         inline constexpr uintptr_t ToRenderView1 = 0x1c8;
+         inline constexpr uintptr_t ServerIP = 0x5c8;
+         inline constexpr uintptr_t ToRenderView1 = 0x1d0;
          inline constexpr uintptr_t ToRenderView2 = 0x8;
          inline constexpr uintptr_t ToRenderView3 = 0x28;
-         inline constexpr uintptr_t Workspace = 0x158;
+         inline constexpr uintptr_t Workspace = 0x160;
     }
 
     namespace DepthOfFieldEffect {
@@ -178,7 +178,7 @@ namespace Offsets {
     }
 
     namespace FakeDataModel {
-         inline constexpr uintptr_t Pointer = 0x8dc2258;
+         inline constexpr uintptr_t Pointer = 0x8e42c98;
          inline constexpr uintptr_t RealDataModel = 0x1f8;
     }
 
@@ -218,7 +218,7 @@ namespace Offsets {
          inline constexpr uintptr_t AutoJumpEnabled = 0x1d4;
          inline constexpr uintptr_t AutoRotate = 0x1d5;
          inline constexpr uintptr_t AutomaticScalingEnabled = 0x1d6;
-         inline constexpr uintptr_t BreakJointsOnDeath = 0x1d7;
+         inline constexpr uintptr_t BreakJointsOnDeath = 0xc5;
          inline constexpr uintptr_t CameraOffset = 0x128;
          inline constexpr uintptr_t DisplayDistanceType = 0x180;
          inline constexpr uintptr_t DisplayName = 0xb8;
@@ -273,7 +273,7 @@ namespace Offsets {
 
     namespace LRUNode {
          inline constexpr uintptr_t AssetID = 0x10;
-         inline constexpr uintptr_t CachedItem = 0x38;
+         inline constexpr uintptr_t CachedItem = 0x40;
          inline constexpr uintptr_t Next = 0x0;
     }
 
@@ -416,14 +416,14 @@ namespace Offsets {
     }
 
     namespace Primitive {
-         inline constexpr uintptr_t AssemblyAngularVelocity = 0x104;
-         inline constexpr uintptr_t AssemblyLinearVelocity = 0xf8;
-         inline constexpr uintptr_t Flags = 0x1b6;
+         inline constexpr uintptr_t AssemblyAngularVelocity = 0xec;
+         inline constexpr uintptr_t AssemblyLinearVelocity = 0xe0;
+         inline constexpr uintptr_t Flags = 0x1be;
          inline constexpr uintptr_t Material = 0x0;
-         inline constexpr uintptr_t Owner = 0x210;
-         inline constexpr uintptr_t Position = 0xec;
-         inline constexpr uintptr_t Rotation = 0xc8;
-         inline constexpr uintptr_t Size = 0x1bc;
+         inline constexpr uintptr_t Owner = 0x218;
+         inline constexpr uintptr_t Position = 0xd4;
+         inline constexpr uintptr_t Rotation = 0xb0;
+         inline constexpr uintptr_t Size = 0x1c4;
          inline constexpr uintptr_t Validate = 0x6;
     }
 
@@ -504,7 +504,7 @@ namespace Offsets {
     }
 
     namespace SpawnLocation {
-         inline constexpr uintptr_t AllowTeamChangeOnTouch = 0x1f0;
+         inline constexpr uintptr_t AllowTeamChangeOnTouch = 0x3d;
          inline constexpr uintptr_t Enabled = 0x1f1;
          inline constexpr uintptr_t ForcefieldDuration = 0x1e8;
          inline constexpr uintptr_t Neutral = 0x1f2;
@@ -517,7 +517,7 @@ namespace Offsets {
     }
 
     namespace StatsItem {
-         inline constexpr uintptr_t Value = 0x1091;
+         inline constexpr uintptr_t Value = 0xd66a;
     }
 
     namespace SunRaysEffect {
@@ -543,7 +543,7 @@ namespace Offsets {
          inline constexpr uintptr_t JobName = 0x18;
          inline constexpr uintptr_t JobStart = 0xc8;
          inline constexpr uintptr_t MaxFPS = 0xb0;
-         inline constexpr uintptr_t Pointer = 0x8b5cee8;
+         inline constexpr uintptr_t Pointer = 0x8bdd8e8;
     }
 
     namespace Team {
@@ -567,9 +567,9 @@ namespace Offsets {
 
     namespace Tool {
          inline constexpr uintptr_t CanBeDropped = 0x4b8;
-         inline constexpr uintptr_t Enabled = 0x4b9;
+         inline constexpr uintptr_t Enabled = 0x475;
          inline constexpr uintptr_t Grip = 0x4ac;
-         inline constexpr uintptr_t ManualActivationOnly = 0x4ba;
+         inline constexpr uintptr_t ManualActivationOnly = 0x295;
          inline constexpr uintptr_t RequiresHandle = 0x4bb;
          inline constexpr uintptr_t TextureId = 0x360;
          inline constexpr uintptr_t Tooltip = 0x468;
@@ -594,7 +594,7 @@ namespace Offsets {
     namespace VisualEngine {
          inline constexpr uintptr_t Dimensions = 0xb10;
          inline constexpr uintptr_t FakeDataModel = 0xaf0;
-         inline constexpr uintptr_t Pointer = 0x83f0a08;
+         inline constexpr uintptr_t Pointer = 0x846f768;
          inline constexpr uintptr_t RenderView = 0xc30;
          inline constexpr uintptr_t ViewMatrix = 0x1b0;
     }
